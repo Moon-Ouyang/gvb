6c176b24d7adf2106b06e8871f6fb3872d6b75e2
“Moon-Ouyang” <995459889@qq.com>
2023-03-10 15:05:17 +0800

    项目搭建+antdv+iconfont引入

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style lang="scss">
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+@import "assets/css/iconfont.css"
+</style>
+
+
